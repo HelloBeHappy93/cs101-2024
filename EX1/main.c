e79621a201d64da8a354a59eb457320303ef04f7
HelloBeHappy93 <155662316+HelloBeHappy93@users.noreply.github.com>
2024-03-14 15:44:31 +0800

    Add files via upload

---
+++ EX1/main.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+using namespace std;
+
+void print_sp(int i,int n)
+{
+    for (i = 0; i < n;i++)
+    {
+        printf(" ");
+    }
+}
+
+void print_num(int n)
+{
+    for (int i = 0; i < n;i++)
+    {
+        printf(n);
+    }
+    printf(" ");
+}
+
+int main()
+{
+    int rows = 6;
+    int number = 1;
+    for (int i = 0; i < rows;i++)
+    {
+        print_sp(number,)
+
+    }
+}
